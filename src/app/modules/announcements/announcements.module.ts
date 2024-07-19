ad3f2057651a9784df6f87b189d5969da2f5c483
Nahi002 <nahomdebele002@gmail.com>
2024-07-19 23:05:36 +0300

    announcment mdouel

--- src/app/modules/announcements/announcements.module.ts
+++ src/app/modules/announcements/announcements.module.ts
@@ -4,10 +4,12 @@ import { Announcement } from './entities/announcement.entity';
 import { AnnouncementsService } from './announcements.service';
 import { AnnouncementsController } from './announcements.controller';
 import { User } from '../users/entities/user.entity';
+import { Ticket } from '../tickets/entities/ticket.entity';
+import { TicketsModule } from '../tickets/tickets.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Announcement , User ])],
-  providers: [AnnouncementsService],
-  controllers: [AnnouncementsController]
+  imports: [TypeOrmModule.forFeature([Announcement ,Ticket, User ]) , TicketsModule],
+  providers: [AnnouncementsService ],
+  controllers: [AnnouncementsController],
 })
 export class AnnouncementsModule {}
