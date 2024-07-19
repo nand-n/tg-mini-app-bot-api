9ea051495818fc64115d8f151bb3506aba6c6fb4
Nahi002 <nahomdebele002@gmail.com>
2024-07-19 21:30:32 +0300

    announcment module

--- src/app/modules/announcements/announcements.module.ts
+++ src/app/modules/announcements/announcements.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Announcement } from './entities/announcement.entity';
 import { AnnouncementsService } from './announcements.service';
 import { AnnouncementsController } from './announcements.controller';
+import { User } from '../users/entities/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Announcement , User ])],
   providers: [AnnouncementsService],
   controllers: [AnnouncementsController]
 })
