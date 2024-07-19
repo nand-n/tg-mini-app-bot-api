656a5a2847c4cf7120e104f49d4e669936320963
Nahi002 <nahomdebele002@gmail.com>
2024-07-19 21:30:39 +0300

    announcment service

--- src/app/modules/announcements/announcements.service.ts
+++ src/app/modules/announcements/announcements.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Announcement } from './entities/announcement.entity';
+import { CreateAnnouncementDto } from './dto/announcment.dto';
+
 
 @Injectable()
-export class AnnouncementsService {}
+export class AnnouncementsService {
+  constructor(
+    @InjectRepository(Announcement)
+    private announcementRepository: Repository<Announcement>,
+  ) {}
+
+  async createAnnouncement(dto: CreateAnnouncementDto): Promise<Announcement> {
+    const announcement = this.announcementRepository.create(dto);
+    return this.announcementRepository.save(announcement);
+  }
+}
