35e55a4b6f8f93a3ec9b712fa337e7c41f121b07
Nahi002 <nahomdebele002@gmail.com>
2024-07-19 21:30:22 +0300

    announcment controller

--- src/app/modules/announcements/announcements.controller.ts
+++ src/app/modules/announcements/announcements.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { AnnouncementsService } from './announcements.service';
+import { CreateAnnouncementDto } from './dto/announcment.dto';
 
 @Controller('announcements')
-export class AnnouncementsController {}
+export class AnnouncementsController {
+  constructor(private readonly announcementService: AnnouncementsService) {}
+
+  @Post()
+  async create(@Body() createAnnouncementDto: CreateAnnouncementDto) {
+    return this.announcementService.createAnnouncement(createAnnouncementDto);
+  }
+}
