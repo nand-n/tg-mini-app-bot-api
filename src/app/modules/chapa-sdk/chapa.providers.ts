05cbab909dd95620051844433925b454f37f7de0
Nahi002 <nahomdebele002@gmail.com>
2024-07-20 23:18:21 +0300

    cahpa provider

---
+++ src/app/modules/chapa-sdk/chapa.providers.ts
@@ -0,0 +1,12 @@
+import { ChapaOptions } from "./interfaces";
+
+import { CHAPA_OPTIONS } from "./constants";
+
+export function createChapaProviders(options: ChapaOptions) {
+  return [
+    {
+      provide: CHAPA_OPTIONS,
+      useValue: options,
+    },
+  ];
+}
