dc0e21129c81edca2d67e098f2dcbc9a532c5142
Nahi002 <nahomdebele002@gmail.com>
2024-07-20 23:21:01 +0300

    initalize interface

---
+++ src/app/modules/chapa-sdk/interfaces/initialize.interface.ts
@@ -0,0 +1,34 @@
+import { SplitType } from "../enums";
+
+interface Subaccount {
+  id: string;
+  split_type?: SplitType;
+  transaction_charge?: number;
+}
+export interface InitializeOptions {
+  first_name: string;
+  last_name: string;
+  email: string;
+  currency: string;
+  amount: string;
+  phone_number: string;
+  tx_ref: string;
+  callback_url?: string;
+  return_url?: string;
+  userId?: string;
+  bookId?: string;
+  customization?: {
+    title?: string;
+    description?: string;
+    logo?: string;
+  };
+  subaccounts?: Subaccount[];
+}
+
+export interface InitializeResponse {
+  message: string;
+  status: string;
+  data: {
+    checkout_url: string;
+  };
+}
