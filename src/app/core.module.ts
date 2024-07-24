c2f367d3b467499d8e5be5968299ff6357d4a727
Nahi002 <nahomdebele002@gmail.com>
2024-07-24 23:40:36 +0300

    core moduel updated

--- src/app/core.module.ts
+++ src/app/core.module.ts
@@ -6,11 +6,12 @@ import { UsersModule } from './modules/users/users.module';
 import { ClientsModule } from './modules/clients/clients.module';
 import { AnnouncementsModule } from './modules/announcements/announcements.module';
 import { TicketsModule } from './modules/tickets/tickets.module';
-import { PaymentModule } from './modules/payment/payment.module';
-import { ChapaModule } from './modules/chapa-sdk';
+// import { PaymentModule } from './modules/payment/payment.module';
+// import { ChapaModule } from './modules/chapa-sdk';
+import { DrawsModule } from './modules/draw/draw.module';
 
 @Global()
 @Module({
-  imports: [PermissionModule, ProductsModule, UsersModule, ClientsModule , AnnouncementsModule , TicketsModule ],
+  imports: [PermissionModule, ProductsModule, UsersModule, ClientsModule , AnnouncementsModule , TicketsModule , DrawsModule],
 })
 export class CoreModule {}
