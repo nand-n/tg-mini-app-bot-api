4e96fa6599cbb0064cfa6ade42ac689ab3c49170
Nahi002 <nahomdebele002@gmail.com>
2024-07-30 21:43:12 +0300

    announcment module

--- src/app/modules/announcements/announcements.module.ts
+++ src/app/modules/announcements/announcements.module.ts
@@ -6,9 +6,10 @@ import { AnnouncementsController } from './announcements.controller';
 import { User } from '../users/entities/user.entity';
 import { Ticket } from '../tickets/entities/ticket.entity';
 import { TicketsModule } from '../tickets/tickets.module';
+import { Draw } from '../draw/entities/draw.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Announcement ,Ticket, User ]) , TicketsModule],
+  imports: [TypeOrmModule.forFeature([Announcement ,Ticket, User , Draw ]) , TicketsModule ],
   providers: [AnnouncementsService ],
   controllers: [AnnouncementsController],
 })
