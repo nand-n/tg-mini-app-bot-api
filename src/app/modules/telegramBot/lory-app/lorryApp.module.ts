43312b15b4f7a6d469d2be3b0b5d65da6e454d72
Nahi002 <nahomdebele002@gmail.com>
2024-08-01 23:31:13 +0300

    tg bot updated

--- src/app/modules/telegramBot/lory-app/lorryApp.module.ts
+++ src/app/modules/telegramBot/lory-app/lorryApp.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
-import { RandomNumberScene } from './scene/random-numbers.scene';
 import { GreeterUpdate } from './lorryApp.update';
-import { GreeterWizard } from './wizard/greeter.wizard';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Announcement } from '../../announcements/entities/announcement.entity';
+import { AnnouncementsService } from '../../announcements/announcements.service';
+import { Ticket } from '../../tickets/entities/ticket.entity';
+import { DrawsController } from '../../draw/draw.controller';
+import { Draw } from '../../draw/entities/draw.entity';
+import { TicketsService } from '../../tickets/tickets.service';
+import { DrawsService } from '../../draw/draw.service';
+import { User } from '../../users/entities/user.entity';
 
 @Module({
-  providers: [GreeterUpdate, RandomNumberScene, GreeterWizard],
+  imports: [
+    TypeOrmModule.forFeature([Announcement,Ticket, Draw , User]), // Include all necessary entities here
+  ],
+  providers: [
+    AnnouncementsService, 
+    TicketsService, 
+    DrawsService,
+    GreeterUpdate
+  ],
+  controllers: [DrawsController],
 })
 export class GreeterModule {}
