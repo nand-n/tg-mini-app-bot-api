78ea5d66a3f3b6aa1501bc77c71673269801fc15
Nahi002 <nahomdebele002@gmail.com>
2024-08-01 23:45:33 +0300

    payment service added

--- src/app/modules/payment/payment.service.ts
+++ src/app/modules/payment/payment.service.ts
@@ -4,6 +4,7 @@ import { Repository } from 'typeorm';
 import { Payment } from './entities/payment.entity';
 import { CreatePaymentDto } from './dto/create-payment.dto';
 import { UpdatePaymentDto } from './dto/update-payment.dto';
+import { CreateEqubPaymentDto } from './dto/create-equb.dto';
 
 @Injectable()
 export class PaymentService {
@@ -38,5 +39,10 @@ export class PaymentService {
   async remove(id: string): Promise<void> {
     const payment = await this.findOne(id);
     await this.paymentRepository.remove(payment);
+  }
+
+  async createEqubPayment(createEqubPaymentDto: CreateEqubPaymentDto): Promise<Payment> {
+    const newEqubPayment = this.paymentRepository.create(createEqubPaymentDto);
+    return await this.paymentRepository.save(newEqubPayment);
   }
 }
