1f9df9960c02a5aae79fe2a0af2efb2892f66e3d
Nahi002 <nahomdebele002@gmail.com>
2024-08-03 23:38:48 +0300

    user entity

--- src/app/modules/users/entities/user.entity.ts
+++ src/app/modules/users/entities/user.entity.ts
@@ -3,20 +3,36 @@ import { Entity, Column, OneToMany } from 'typeorm';
 import { Ticket } from '../../tickets/entities/ticket.entity';
 import { IsOptional } from 'class-validator';
 import { Payment } from '../../payment/entities/payment.entity';
+import { Role } from '../enums/role.enum';
+import { Permission, PermissionType } from '../../auth/autherization/permission.type';
 
 @Entity()
 export class User extends BaseModel {
   @Column({ length: 500, type: 'varchar' })
   name: string;
+  @Column({ length: 500, type: 'varchar' })
+  password: string;
 
-  @Column({length:10 , type: 'varchar' , default:'player' , enum:["player" , "admin"]})
-  role:string
+  // @Column({length:10 , type: 'varchar' , default:'player' , enum:["player" , "admin" , "superadmin"]})
+  // role:string
 
-  @Column({ length: 50, type: 'varchar' })
+  // @Column({ enum: ["create" , "update" , ""], default: [], type: "json" })
+  // permissions: PermissionType[];
+
+  @Column({ enum: Role, default: Role.Regular })
+  role: Role;
+
+  @Column({ enum: Permission, default: [], type: "json" })
+  permissions: PermissionType[];
+
+  @Column({ length: 50, type: 'varchar' ,nullable:true})
   phone: string;
 
-  @Column({ length: 50, type: 'varchar' })
+  @Column({ length: 50, type: 'varchar', nullable:true })
   email: string;
+
+  @Column({ length: 50, type: 'varchar', nullable:true })
+  telegramUser: string;
 
   @OneToMany(() => Ticket, (ticket) => ticket.player,  { nullable: true })
   tickets: Ticket[];
