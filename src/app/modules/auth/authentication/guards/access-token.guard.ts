447e303cd0620e4f94d95ac90a3b6ade55c853d5
Nahi002 <nahomdebele002@gmail.com>
2024-08-04 23:10:37 +0300

    access token guard updated

--- src/app/modules/auth/authentication/guards/access-token.guard.ts
+++ src/app/modules/auth/authentication/guards/access-token.guard.ts
@@ -1,45 +1,108 @@
+// import {
+//     CanActivate,
+//     ExecutionContext,
+//     Inject,
+//     Injectable,
+//     UnauthorizedException,
+//     forwardRef,
+//   } from "@nestjs/common";
+//   import { ConfigType } from "@nestjs/config";
+//   import { JwtService } from "@nestjs/jwt";
+//   import { Request } from "express";
+// import { REQUEST_USER, REQUEST_USER_KEY } from "../../auth.constants";
+// import jwtConfig from "../../config/jwt.config";
+// import { UsersService } from "../../../users/users.service";
+  
+//   @Injectable()
+//   export class AccessTokenGuard implements CanActivate {
+//     constructor(
+//       private readonly jwtService: JwtService,
+//       @Inject(jwtConfig.KEY)
+//       private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
+//       @Inject(forwardRef(() => UsersService)) private readonly userService: UsersService,
+//     ) {}
+  
+//     async canActivate(context: ExecutionContext): Promise<boolean> {
+//       const request = context.switchToHttp().getRequest();
+//       const token = this.extractTokenFromHeader(request);
+//       if (!token) {
+//         throw new UnauthorizedException("Token not found");
+//       }
+//       try {
+//         const payload = await this.jwtService.verifyAsync(
+//           token,
+//           this.jwtConfiguration,
+//         );
+//         request.REQUEST_USER_KEY = payload;
+//         const userId = payload.sub;
+//       const user = await this.userService.findOne(userId);
+//       if (!user) {
+//         throw new UnauthorizedException("User not found");
+//       }
+//       request[REQUEST_USER] = user;
+//       } catch (err) {
+//         throw new UnauthorizedException("Token not valid");
+//       }
+//       return true;
+//     }
+  
+//     private extractTokenFromHeader(request: Request): string | undefined {
+//       // eslint-disable-next-line @typescript-eslint/no-unused-vars
+//       const [_, token] = request.headers.authorization?.split(" ") ?? [];
+//       return token;
+//     }
+//   }
+
 import {
-    CanActivate,
-    ExecutionContext,
-    Inject,
-    Injectable,
-    UnauthorizedException,
-  } from "@nestjs/common";
-  import { ConfigType } from "@nestjs/config";
-  import { JwtService } from "@nestjs/jwt";
-  import { Request } from "express";
-import { REQUEST_USER_KEY } from "../../auth.constants";
-import jwtConfig from "../../config/jwt.config";
-  
-  @Injectable()
-  export class AccessTokenGuard implements CanActivate {
-    constructor(
-      private readonly jwtService: JwtService,
-      @Inject(jwtConfig.KEY)
-      private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
-    ) {}
-  
-    async canActivate(context: ExecutionContext): Promise<boolean> {
-      const request = context.switchToHttp().getRequest();
-      const token = this.extractTokenFromHeader(request);
-      if (!token) {
-        throw new UnauthorizedException("Token not found");
-      }
-      try {
-        const payload = await this.jwtService.verifyAsync(
-          token,
-          this.jwtConfiguration,
-        );
-        request[REQUEST_USER_KEY] = payload;
-      } catch (err) {
-        throw new UnauthorizedException("Token not valid");
-      }
-      return true;
+  CanActivate,
+  ExecutionContext,
+  Inject,
+  Injectable,
+  UnauthorizedException,
+  forwardRef,
+} from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import { REQUEST_USER, REQUEST_USER_KEY } from '../../auth.constants';
+import jwtConfig from '../../config/jwt.config';
+import { UsersService } from '../../../users/users.service';
+
+@Injectable()
+export class AccessTokenGuard implements CanActivate {
+  constructor(
+    private readonly jwtService: JwtService,
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
+    @Inject(forwardRef(() => UsersService)) private readonly userService: UsersService,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      throw new UnauthorizedException('Token not found');
     }
-  
-    private extractTokenFromHeader(request: Request): string | undefined {
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const [_, token] = request.headers.authorization?.split(" ") ?? [];
-      return token;
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: this.jwtConfiguration.secret,
+      });
+      request.REQUEST_USER_KEY = payload;
+      const userId = payload.sub;
+      const user = await this.userService.findOne(userId);
+      if (!user) {
+        throw new UnauthorizedException('User not found');
+      }
+      request[REQUEST_USER] = user;
+    } catch (err) {
+      throw new UnauthorizedException('Token not valid');
     }
+    return true;
   }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [_, token] = request.headers.authorization?.split(' ') ?? [];
+    return token;
+  }
+}
