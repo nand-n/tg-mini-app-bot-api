2575018dcd461987b1a367fcf9af68e758ab2644
Nahi002 <nahomdebele002@gmail.com>
2024-08-04 23:10:15 +0300

    sign up dto updated

--- src/app/modules/auth/authentication/dto/sign-up.dto.ts
+++ src/app/modules/auth/authentication/dto/sign-up.dto.ts
@@ -1,9 +1,32 @@
-import { IsEmail, MinLength } from "class-validator";
+import { IsEmail, IsString, IsOptional, IsEnum, IsArray } from 'class-validator';
+import { Role } from '../../../users/enums/role.enum';
+import { Permission, PermissionType } from '../../autherization/permission.type';
 
 export class SignUpDto {
-  @IsEmail()
-  email: string;
+  @IsString()
+  name: string;
 
-  @MinLength(10)
+  @IsString()
   password: string;
+
+  @IsEmail()
+  @IsOptional()
+  email?: string;
+
+  @IsString()
+  @IsOptional()
+  phone?: string;
+
+  @IsString()
+  @IsOptional()
+  telegramUser?: string;
+
+  @IsEnum(Role)
+  @IsOptional()
+  role?: Role = Role.Regular;
+
+  @IsArray()
+  @IsEnum(Permission, { each: true })
+  @IsOptional()
+  permissions?: PermissionType[] = [];
 }
