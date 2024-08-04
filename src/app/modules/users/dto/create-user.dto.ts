821a6896ab7134277758ae91ecdf7b278bb25626
Nahi002 <nahomdebele002@gmail.com>
2024-08-04 23:13:13 +0300

    create user dto updated

--- src/app/modules/users/dto/create-user.dto.ts
+++ src/app/modules/users/dto/create-user.dto.ts
@@ -1,9 +1,32 @@
-import { IsEmail, IsString } from 'class-validator';
+import { IsEmail, IsString, IsOptional, IsEnum, IsArray } from 'class-validator';
+import { Role } from '../enums/role.enum';
+import { Permission, PermissionType } from '../../auth/autherization/permission.type';
 
 export class CreateUserDto {
   @IsString()
   name: string;
 
+  @IsString()
+  password: string;
+
   @IsEmail()
-  email: string;
+  @IsOptional()
+  email?: string;
+
+  @IsString()
+  @IsOptional()
+  phone?: string;
+
+  @IsString()
+  @IsOptional()
+  telegramUser?: string;
+
+  @IsEnum(Role)
+  @IsOptional()
+  role?: Role = Role.Regular;
+
+  @IsArray()
+  @IsEnum(Permission, { each: true })
+  @IsOptional()
+  permissions?: PermissionType[] = [];
 }
