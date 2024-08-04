266c9872e02ed0ba9b1a27dd8b85ee1b9ad670aa
Nahi002 <nahomdebele002@gmail.com>
2024-08-04 23:11:26 +0300

    user module updated

--- src/app/modules/users/users.module.ts
+++ src/app/modules/users/users.module.ts
@@ -1,14 +1,18 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { PaginationService } from '../../../core/pagination/pagination.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Product } from '../products/entities/product.entity';
+import { AuthModule } from '../auth/auth.module';
+import jwtConfig from '../auth/config/jwt.config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Product ])],
+  imports: [TypeOrmModule.forFeature([User, Product ]) , forwardRef(() => AuthModule) ,  ConfigModule.forFeature(jwtConfig),],
   controllers: [UsersController],
   providers: [UsersService, PaginationService],
+  exports: [UsersService],
 })
 export class UsersModule {}
