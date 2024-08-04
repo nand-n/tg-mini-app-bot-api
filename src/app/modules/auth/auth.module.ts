c44a0ff779f109094790b44689372edbb2a46f31
Nahi002 <nahomdebele002@gmail.com>
2024-08-04 23:09:51 +0300

    auth moudle updated

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,10 +1,10 @@
-import { Module } from "@nestjs/common";
+import { Module, forwardRef } from "@nestjs/common";
 import { BcryptService } from "./hashing/bcrypt.service";
 import { HashingService } from "./hashing/hashing.service";
 import { AuthenticationController } from "./authentication/authentication.controller";
 import { AuthenticationService } from "./authentication/authentication.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { JwtModule } from "@nestjs/jwt";
+import { JwtModule, JwtService } from "@nestjs/jwt";
 import jwtConfig from "./config/jwt.config";
 import { ConfigModule } from "@nestjs/config";
 import { APP_GUARD } from "@nestjs/core";
@@ -14,30 +14,36 @@ import { User } from "../users/entities/user.entity";
 import { RolesGuard } from "./autherization/guards/roles.guard";
 import { RefreshTokenIdsStorage } from "./authentication/refres-token-ids.storage";
 import { RefreshToken } from "./authentication/entities/refres-token.entity";
+import { UsersModule } from "../users/users.module";
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User , RefreshToken]),
     JwtModule.registerAsync(jwtConfig.asProvider()),
     ConfigModule.forFeature(jwtConfig),
+    forwardRef(() => UsersModule),
   ],
+
   providers: [
     {
       provide: HashingService,
       useClass: BcryptService,
     },
-    {
-      provide: APP_GUARD,
-      useClass: AuthenticationGuard,
-    },
-    {
-      provide: APP_GUARD,
-      useClass: RolesGuard,
-    },
+    // {
+    //   provide: APP_GUARD,
+    //   useClass: AuthenticationGuard,
+    // },
+    // {
+    //   provide: APP_GUARD,
+    //   useClass: RolesGuard,
+    // },
     RefreshTokenIdsStorage,
     AccessTokenGuard,
     AuthenticationService,
+    RolesGuard, 
+    JwtService,
   ],
   controllers: [AuthenticationController],
+  exports: [AccessTokenGuard, RolesGuard, JwtService],
 })
 export class AuthModule {}
