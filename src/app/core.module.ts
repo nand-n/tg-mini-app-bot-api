8df3f395966c4f620c3b3169f2341c9ce9810438
Nahi002 <nahomdebele002@gmail.com>
2024-08-04 23:08:58 +0300

    core modules

--- src/app/core.module.ts
+++ src/app/core.module.ts
@@ -6,12 +6,9 @@ import { UsersModule } from './modules/users/users.module';
 import { ClientsModule } from './modules/clients/clients.module';
 import { AnnouncementsModule } from './modules/announcements/announcements.module';
 import { TicketsModule } from './modules/tickets/tickets.module';
-// import { PaymentModule } from './modules/payment/payment.module';
-// import { ChapaModule } from './modules/chapa-sdk';
 import { DrawsModule } from './modules/draw/draw.module';
 import { TelegramBotModule } from './modules/telegramBot/telegram.module';
 import { ChapaModule } from './modules/chapa-sdk';
-import { AuthModule } from './modules/auth/auth.module';
 
 @Global()
 @Module({
