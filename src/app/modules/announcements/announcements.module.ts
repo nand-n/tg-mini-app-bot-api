1f99d721d3a8fbeb5f96b69eee6b849befd2c434
Nahi002 <nahomdebele002@gmail.com>
2024-08-05 23:37:59 +0300

    fixing the role and permistion

--- src/app/modules/announcements/announcements.module.ts
+++ src/app/modules/announcements/announcements.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Announcement } from './entities/announcement.entity';
 import { AnnouncementsService } from './announcements.service';
@@ -8,9 +8,11 @@ import { Ticket } from '../tickets/entities/ticket.entity';
 import { TicketsModule } from '../tickets/tickets.module';
 import { Draw } from '../draw/entities/draw.entity';
 import { AuthModule } from '../auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import jwtConfig from '../auth/config/jwt.config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Announcement ,Ticket, User , Draw ]) , TicketsModule ],
+  imports: [TypeOrmModule.forFeature([Announcement ,Ticket, User , Draw ]) , TicketsModule],
   providers: [AnnouncementsService ],
   controllers: [AnnouncementsController],
 })
