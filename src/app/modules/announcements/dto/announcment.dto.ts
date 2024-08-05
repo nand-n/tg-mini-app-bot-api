2559de47d6c5d99ea56d49c5130683806ae9abb3
Nahi002 <nahomdebele002@gmail.com>
2024-08-05 23:38:15 +0300

    fixing the annoucnemetn role and permistion

--- src/app/modules/announcements/dto/announcment.dto.ts
+++ src/app/modules/announcements/dto/announcment.dto.ts
@@ -1,6 +1,4 @@
-// import { Transform } from 'class-transformer';
 import { IsString, IsInt, IsDateString,  } from 'class-validator';
-// import dayjs from 'dayjs';
 
 export class CreateAnnouncementDto {
   @IsString()
@@ -10,7 +8,6 @@ export class CreateAnnouncementDto {
   endDate: string;
 
   @IsString()
-  // @Transform(({ value }) => dayjs(value).format('HH:mm:ss'), { toClassOnly: true })
   endTime: string;
 
   @IsInt()
