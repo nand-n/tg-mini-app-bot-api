ae75d61f32b67d1776159810506e83bd55534c27
Nahi002 <nahomdebele002@gmail.com>
2024-08-05 23:37:49 +0300

    fixing the role and permision

--- src/app/modules/announcements/announcements.controller.ts
+++ src/app/modules/announcements/announcements.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Post, Body, Get, Param } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, UseGuards } from '@nestjs/common';
 import { AnnouncementsService } from './announcements.service';
 import { CreateAnnouncementDto } from './dto/announcment.dto';
 import { Roles } from '../auth/autherization/decorators/role.decorator';
 import { Role } from '../users/enums/role.enum';
+import { AccessTokenGuard } from '../auth/authentication/guards/access-token.guard';
+import { RolesGuard } from '../auth/autherization/guards/roles.guard';
 
 @Controller('announcements')
 export class AnnouncementsController {
@@ -10,10 +12,13 @@ export class AnnouncementsController {
 
   @Post()
   @Roles(Role.Admin)
+  // @UseGuards(AccessTokenGuard)
   async create(@Body() createAnnouncementDto: CreateAnnouncementDto) {
     return this.announcementService.create(createAnnouncementDto);
   }
   @Get()
+  @Roles(Role.Admin)
+  @UseGuards(RolesGuard)
   async findAll() {
     return this.announcementService.findAll();
   }
