1398740e0dd3ae6e6cd3e3c53a5039840d866469
Nahi002 <nahomdebele002@gmail.com>
2024-08-07 22:47:40 +0300

    announmcent entity updated

--- src/app/modules/announcements/entities/announcement.entity.ts
+++ src/app/modules/announcements/entities/announcement.entity.ts
@@ -1,6 +1,7 @@
-import { Entity, Column, OneToMany } from 'typeorm';
+import { Entity, Column, OneToMany, ManyToOne, JoinColumn } from 'typeorm';
 import { BaseModel } from '@root/src/database/base.model';
 import { Ticket } from '../../tickets/entities/ticket.entity';
+import { User } from '../../users/entities/user.entity';
 
 @Entity('announcements')
 export class Announcement  extends BaseModel {
@@ -18,4 +19,8 @@ export class Announcement  extends BaseModel {
 
   @OneToMany(() => Ticket, ticket => ticket.announcement, { nullable: true })
   tickets: Ticket[];
+  @ManyToOne(() => User, user => user.ticketsCreatedBy, { nullable: false })
+  @JoinColumn({ name: 'createdBy' })
+  createdBy: User;
+  
 }
