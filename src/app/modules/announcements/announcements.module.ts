96e0f8e865b46b1765a6ee83676ba99b8f1be315
Nahi002 <nahomdebele002@gmail.com>
2024-08-07 22:47:18 +0300

    announcment module

--- src/app/modules/announcements/announcements.module.ts
+++ src/app/modules/announcements/announcements.module.ts
@@ -8,12 +8,18 @@ import { Ticket } from '../tickets/entities/ticket.entity';
 import { TicketsModule } from '../tickets/tickets.module';
 import { Draw } from '../draw/entities/draw.entity';
 import { AuthModule } from '../auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
 import jwtConfig from '../auth/config/jwt.config';
+import { ConfigModule } from '@nestjs/config';
+import { UsersService } from '../users/users.service';
+import { UsersModule } from '../users/users.module';
+import { PaginationService } from '@root/src/core/pagination/pagination.service';
+import { RolesGuardService } from '@root/src/core/middlewares/roleMiddleware';
+
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Announcement ,Ticket, User , Draw ]) , TicketsModule],
-  providers: [AnnouncementsService ],
+  imports: [TypeOrmModule.forFeature([Announcement ,Ticket, User , Draw ]) , TicketsModule,  forwardRef(() => AuthModule) ,  TicketsModule ,  ConfigModule.forFeature(jwtConfig) ],
+  providers: [AnnouncementsService  ,UsersService , PaginationService ],
   controllers: [AnnouncementsController],
+  exports:[AnnouncementsService]
 })
 export class AnnouncementsModule {}
