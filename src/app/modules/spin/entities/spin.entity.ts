5a4f4f3434993850130d75e6f73fc58efaea8ef6
Nahi002 <nahomdebele002@gmail.com>
2024-08-19 23:08:49 +0300

    spin entity

--- src/app/modules/spin/entities/spin.entity.ts
+++ src/app/modules/spin/entities/spin.entity.ts
@@ -1,18 +1,18 @@
 // spin.entity.ts
-import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+import { Entity, Column, ManyToOne, JoinColumn } from 'typeorm';
 import { User } from '../../users/entities/user.entity';
+import { BaseModel } from '@root/src/database/base.model';
+import { Segment } from '../../spin-segments/entities/segment.entity';
 
 @Entity()
-export class Spin {
-  @PrimaryGeneratedColumn()
-  id: number;
-
-  @Column()
-  result: string;
+export class Spin extends BaseModel {
 
   @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
   spinDate: Date;
 
   @ManyToOne(() => User, user => user.spins, { onDelete: 'CASCADE' })
   user: User;
+
+  @ManyToOne(() => Segment)
+  result: Segment;
 }
