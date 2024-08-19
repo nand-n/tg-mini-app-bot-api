b44060532bd76fdcecf30f852fb4c448911dbfc8
Nahi002 <nahomdebele002@gmail.com>
2024-08-19 23:08:18 +0300

    spin the wheal moduel

--- src/app/modules/spin/spin.module.ts
+++ src/app/modules/spin/spin.module.ts
@@ -4,11 +4,14 @@ import { Spin } from './entities/spin.entity';
 import { User } from '../users/entities/user.entity';
 import { SpinTheWheelService } from './spin.service';
 import { SpinTheWheelController } from './spin.controller';
+import { Segment } from '../spin-segments/entities/segment.entity';
+import { SegmentService } from '../spin-segments/segment.service';
 
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Spin, User])],
-  providers: [SpinTheWheelService],
+  imports: [TypeOrmModule.forFeature([Spin, User, Segment])],
+  providers: [SpinTheWheelService , SegmentService],
   controllers: [SpinTheWheelController],
+  exports:[SpinTheWheelService ]
 })
 export class SpinTheWheelModule {}
