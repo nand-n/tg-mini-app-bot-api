0c883a2e2b13bdc78f68c21cd1520f929d4eb223
Nahi002 <nahomdebele002@gmail.com>
2024-08-28 23:46:42 +0300

    spin entith update

--- src/app/modules/spin/entities/spin.entity.ts
+++ src/app/modules/spin/entities/spin.entity.ts
@@ -1,7 +1,8 @@
-import { Entity, Column, ManyToOne, JoinColumn } from 'typeorm';
+import { Entity, Column, ManyToOne, JoinColumn, OneToOne } from 'typeorm';
 import { User } from '../../users/entities/user.entity';
 import { BaseModel } from '@root/src/database/base.model';
 import { Segment } from '../../spin-segments/entities/segment.entity';
+import { SpinTicket } from './spin-ticket.entity';
 
 @Entity()
 export class Spin extends BaseModel {
@@ -11,11 +12,16 @@ export class Spin extends BaseModel {
 
   @ManyToOne(() => User, user => user.spins, { onDelete: 'CASCADE' })
   user: User;
-  
+
   @Column({ default: 0 })
   ticketCount: number; 
 
   @ManyToOne(() => Segment, { nullable: true })
   @JoinColumn({ name: 'result_id' }) 
   result: Segment;
+  
+
+  @OneToOne(() => SpinTicket, ticket => ticket.spin, { nullable: true })
+  @JoinColumn()
+  ticket: SpinTicket; 
 }
