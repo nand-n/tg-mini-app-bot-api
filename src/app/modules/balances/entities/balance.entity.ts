eaaaf251eeadef6b199e2d4cf6c1abb968715143
Nahi002 <nahomdebele002@gmail.com>
2024-08-31 22:43:56 +0300

    balance entity

--- src/app/modules/balances/entities/balance.entity.ts
+++ src/app/modules/balances/entities/balance.entity.ts
@@ -1,12 +1,15 @@
-import { Entity, Column, OneToOne } from 'typeorm';
+import { Entity, Column, OneToOne, JoinColumn } from 'typeorm';
 import { BaseModel } from '@root/src/database/base.model';
 import { User } from '../../users/entities/user.entity';
 
 @Entity()
 export class Balance extends BaseModel {
-  @OneToOne(() => User, (user) => user.balance)
-  user: User;
+  // @OneToOne(() => User, (user) => user.balance)
+  // user: User;
 
+  @OneToOne(() => User, (user) => user.balance)
+  @JoinColumn({ name: 'userId' })
+  user: User;
   @Column({ type: 'decimal', default: 0 })
   diceBalance: number;
 
