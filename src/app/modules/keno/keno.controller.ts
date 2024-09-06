ea555da93d9056bcd4f90ebc489ef5012cfce5cd
Nahi002 <nahomdebele002@gmail.com>
2024-09-06 23:13:08 +0300

    keno controller

---
+++ src/app/modules/keno/keno.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Post, Body, Param, Get } from '@nestjs/common';
+import { KenoService } from './keno.service';
+import { PlayKenoGameDto } from './dto/play-keno-game.dto';
+
+@Controller('keno')
+export class KenoController {
+  constructor(private readonly kenoService: KenoService) {}
+
+  @Post(':userId/play')
+  async playKenoGame(
+    @Param('userId') userId: string,
+    @Body() playKenoGameDto: PlayKenoGameDto
+  ) {
+    const { selectedNumbers, betAmount } = playKenoGameDto;
+    return this.kenoService.playKenoGame(userId, selectedNumbers, betAmount);
+  }
+
+  @Get(':userId/games')
+  async getUserKenoGames(@Param('userId') userId: string) {
+    return this.kenoService.getUserKenoGames(userId);
+  }
+}
